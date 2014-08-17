1b4ce6dd7d2c34461daea0d4a66869484411cae5
GitMyCode <marbouchl@gmail.com>
2014-08-17 01:38:23 -0400

    hedgemony fini

--- Code-Jam-for-Veterans-2013/Hedgemony/src/main/scala/Hedgemony.scala
+++ Code-Jam-for-Veterans-2013/Hedgemony/src/main/scala/Hedgemony.scala
@@ -1,4 +1,4 @@
-import java.io.FileReader
+import java.io.{FileWriter, FileReader}
 import java.util
 import java.util.Scanner
 
@@ -8,35 +8,40 @@ import java.util.Scanner
 object Hedgemony {
 
   val SAMPLE = "src/main/scala/sample"
+  val SMALL = "src/main/scala/A-small-practice.in"
+  val LARGE = "src/main/scala/A-large-practice.in"
+
+  val OUT = "src/main/scala/output"
 
 
   var N:Int = _
-  var A:Array[Int]= _
+  var A:Array[Double]= _
   def main(args: Array[String]){
 
-    print("sdf")
-    val in = new Scanner(new FileReader(SAMPLE))
-
+    val in = new Scanner(new FileReader(LARGE))
+    val out = new FileWriter(OUT)
     val nb_cases = in.nextInt()
     for( i <- 0 until nb_cases){
 
       N = in.nextInt()
-      A = new Array[Int](N)
+      A = new Array[Double](N)
 
       //A = line.split(" ").map(_.toInt)
       for( a <- 0 until N){
         A(a) = in.nextInt()
       }
       solve(A)
-      println("reponse : "+ A(N-2))
-
-      println(util.Arrays.toString(A) )
+      println("Case #"+(i+1)+": "+ A(N-2))
+      out.write("Case #"+(i+1)+": "+ A(N-2) + "\n")
+      //println(util.Arrays.toString(A) )
     }
 
+    in.close()
+    out.close()
   }
 
 
-  def solve(row:Array[Int]){
+  def solve(row:Array[Double]){
     for(i <- 1 until row.length -1){
       val left = row(i-1)
       val right = row(i+1)
