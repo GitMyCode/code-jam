6d446583c22c9949287393cb114fd74abfb54ce7
Martin Bouchard <marbouchl@gmail.com>
2015-03-15 01:24:13 -0400

    Debut Round1c2009

---
+++ Round1c2009/AllYourBase/AllYourBase/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllYourBase
+{
+    class Program
+    {
+        private static string allDigits = "0123456789abcdefghijklmnopqrstuvwxyz";
+        static Dictionary<char, int> DicDigits = new Dictionary<char, int>();
+        static void Main(string[] args)
+        {
+
+
+            var lines = File.ReadLines("./sample.txt");
+            foreach (var line in lines)
+            {
+                string ans = Answer(line);
+                Console.WriteLine(ans);
+            }
+
+
+            Console.Read();
+
+        }
+
+        static string Answer(string line)
+        {
+            DicDigits.Clear();
+            var nbDifferent = 0;
+            foreach (char c in line)
+            {
+                if (DicDigits.ContainsKey(c))
+                {
+                }
+                else
+                {
+                    DicDigits[c] = nbDifferent++;
+                }
+            }
+
+
+            return "";
+        }
+
+    }
+}
