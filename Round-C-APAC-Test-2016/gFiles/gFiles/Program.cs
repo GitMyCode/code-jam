cce8f159c5b7ccc85b08015167dea84279729037
Martin Bouchard <marbouchl@gmail.com>
2015-11-08 00:11:25 -0500

    gFiles (marche juste le small data)

---
+++ Round-C-APAC-Test-2016/gFiles/gFiles/Program.cs
@@ -0,0 +1,110 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gFiles
+{
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            using (var reader = File.OpenText("B-small-practice.in"))
+            {
+                using (var writer = new StreamWriter("answer"))
+                {
+                    var line = reader.ReadLine();
+                    var caseCount = int.Parse(line);
+                    for (int caseNumber = 1; caseNumber <= caseCount; caseNumber++)
+                    {
+                        line = reader.ReadLine();
+                        var updateStatusCount = int.Parse(line);
+
+                        var ranges = new List<dynamic>();
+                        for (int i = 0; i < updateStatusCount; i++)
+                        {
+                            var splitedLine = reader.ReadLine().Split(' ');
+                            var percent = float.Parse(splitedLine[0]);
+                            var fileCount = ulong.Parse(splitedLine[1]);
+                            if (percent == 100)
+                            {
+                                ranges.Clear();
+                                ranges.Add(new { lower = (ulong)fileCount, higher = (ulong)fileCount });
+
+                            }
+                            else if (percent == 0)
+                            {
+
+                            }
+                            else
+                            {
+                                var lowerRange = (fileCount * 100) / (percent + 1) + 1;
+                                var higherRange = (fileCount * 100) / percent;
+                                ranges.Add(new { lower = (ulong)lowerRange, higher = (ulong)higherRange });
+                            }
+
+
+
+                        }
+
+                        var low = ulong.MinValue;
+                        var top = ulong.MaxValue;
+                        // Console.WriteLine("\n---");
+                        //  Console.WriteLine("Case: " + caseNumber);
+                        foreach (var range in ranges)
+                        {
+                            //    Console.WriteLine("low: " + range.lower + "  high: " + range.higher);
+                            if (low < range.lower)
+                            {
+                                low = range.lower;
+                            }
+
+                            if (top > range.higher)
+                            {
+                                top = range.higher;
+                            }
+                        }
+
+                        ulong fileTotal = low;
+                        if (low != top)
+                        {
+                            writer.Write("Case #" + caseNumber + ": -1\n");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Case #" + caseNumber + ": " + fileTotal);
+                            writer.Write("Case #" + caseNumber + ": " + fileTotal + "\n");
+                        }
+
+                        
+                    }
+
+                    //       Console.ReadLine();
+                }
+            }
+        }
+    }
+}
